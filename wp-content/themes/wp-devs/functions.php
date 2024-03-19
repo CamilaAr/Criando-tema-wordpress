709819289d8f3e5ddd3c0f33e3a5387985e4f8d0
Camila Araujo <camila.araujo.rp@hotmail.com>
2024-03-19 15:54:14 -0300

    continuação

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -1,17 +1,24 @@
 <?php
 
-function wpdevs_load_scripts()
-{
-    //carregar estilo(nome da função - colocar o nome do tema, caminho do arquivo, css pai, que nosso css aqui depende, versão, para todos)
-    wp_enqueue_style('wpdevs-style', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css'), 'all');
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null);
-    //handle do arquivo(identificador), caminho, dependencias, versão, e se será carregado por padrão
-    wp_enqueue_script('dropdown', get_template_directory_uri() . '/js/dropdown.js', array(), '1.0', true);
+function wpdevs_load_scripts(){
+    wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null );
+    wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/js/dropdown.js', array(), '1.0', true );
 }
-add_action('wp_enqueue_scripts', 'wpdevs_load_scripts');
-register_nav_menus(
-    array(
-        'wp_devs_main_menu' => 'Main Menu',
-        'wp_devs_footer_menu' => 'Footer Menu'
-    )
-);
+add_action( 'wp_enqueue_scripts', 'wpdevs_load_scripts' );
+
+function wpdevs_config(){
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu' => 'Main Menu',
+            'wp_devs_footer_menu' => 'Footer Menu'
+        )
+    );
+
+    $args = array(
+        'height'    => 225,
+        'width'     => 1920
+    );
+    add_theme_support( 'custom-header', $args );
+}
+add_action( 'after_setup_theme', 'wpdevs_config', 0 );
--- wp-content/themes/wp-devs/header.php
+++ wp-content/themes/wp-devs/header.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
-
     <div id="page" class="site">
         <header>
             <section class="top-bar">
@@ -17,13 +15,12 @@
                         Logo
                     </div>
                     <div class="searchbox">
-                        search
-                    </div>
+                        Search
+                    </div>                    
                 </div>
-
             </section>
             <section class="menu-area">
-                <div class="contanier">
+                <div class="container">
                     <nav class="main-menu">
                         <button class="check-button">
                             <div class="menu-icon">
@@ -32,13 +29,8 @@
                                 <div class="bar3"></div>
                             </div>
                         </button>
-                        <?php wp_nav_menu(array(
-                            'theme_location' => 'wp_devs_main_menu',
-                            'depth' => 2
-                        )); ?>
-
-                    </nav>
+                        <?php wp_nav_menu( array( 'theme_location' => 'wp_devs_main_menu', 'depth' => 2 )); ?>
+                    </nav>                    
                 </div>
-
             </section>
         </header>
